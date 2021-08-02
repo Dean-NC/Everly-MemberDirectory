9e09aa0dba7af262a2a630e2a2e3919661676157
Dean Jackson <12361341+Dean-NC@users.noreply.github.com>
2021-08-02 14:28:38 -0400

    RepositoryBase - open db connections and see if error number is duplicate key related.

--- Source/MemberDirectory.Data/Repositories/RepositoryBase.cs
+++ Source/MemberDirectory.Data/Repositories/RepositoryBase.cs
@@ -2,12 +2,36 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace MemberDirectory.Data.Repositories
 {
+    /// <summary>
+    /// Base class for all data repositories. Provides functionality common to all repositories.
+    /// </summary>
     public class RepositoryBase
     {
+        private readonly DbConfig _dbConfig;
+
+        public RepositoryBase(DbConfig dbConfig)
+        {
+            _dbConfig = dbConfig;
+        }
+
+        /// <summary>
+        /// Returns an open connection to the database.
+        /// </summary>
+        protected async Task<IDbConnection> GetDbConnectionAsync()
+        {
+            // This application will use SQL Server for the database, but we could use another.
+
+            SqlConnection connection = new(_dbConfig.ConnectionString);
+            await connection.OpenAsync();
+
+            return connection;
+        }
 
         /// <summary>
         /// To pass a collection of primitive values to a SQL query or procedure, you can use a .Net DataTable or a
@@ -30,6 +54,15 @@ namespace MemberDirectory.Data.Repositories
                 record.SetInt32(0, id);
                 yield return record;
             }
+        }
+
+        protected static bool IsDuplicateError(int dbErrorNumber)
+        {
+            // Determines if the given error number is because of duplicate key or unique constraint violation.
+            // The numbers are documented here (old article but applys to latest server versions):
+            // https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008-r2/cc645728(v=sql.105)
+
+            return dbErrorNumber == 2601 || dbErrorNumber == 2627;
         }
     }
 }
